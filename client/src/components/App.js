b17272ad9a4b8706ef1f365e429ea7a4bf93576e
Leah Miller <leahmiller1969@gmail.com>
2017-11-09 15:42:17 -0800

    basic web layout complete

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import 'styles/App.css'
 import Header from 'components/Header'
+import Main from 'components/Main'
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
+        <Main />
       </div>
     )
   }
